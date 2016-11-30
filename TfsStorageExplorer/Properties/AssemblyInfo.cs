fa4cf421b6a1831952e3ef1929ffdec983a81804
Jelle Druyts <jelle_druyts@yahoo.com>
2016-11-30 16:52:07 +0100

    Updated version number

--- TfsStorageExplorer/Properties/AssemblyInfo.cs
+++ TfsStorageExplorer/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyVersion("1.0.*")] // This can be updated anytime as long as .NET user settings are not used (otherwise the settings directory depends on this version).
+[assembly: AssemblyVersion("1.1.*")] // This can be updated anytime as long as .NET user settings are not used (otherwise the settings directory depends on this version).
 [assembly: AssemblyInformationalVersion("1.0.0.0")] // Keep this constant as long as possible to avoid the user's settings getting lost (it is used for the LocalUserAppDataPath where the configuration file is stored).
 
 [assembly: AssemblyProduct("TFS Storage Explorer")]
